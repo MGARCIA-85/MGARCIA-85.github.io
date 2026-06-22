109e253b46f62ae4030efbc6199da3637758249a
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-06-22 12:28:24 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'gestor-cgs-v400';
-const FILES = ['./GestorPedidosCGS.html', './manifest.json'];
+const CACHE = 'gestor-cgs-v401';
+const FILES = ['./GestorPedidosCGS.html', './manifest.json', './GP_activ.png', './GP_spalsh.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
