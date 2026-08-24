a80107417b2528c047ce8decf34cb9888b0a5cf0
MGARCIA-85 <massimo.garcia85@gmail.com>
2026-08-24 12:18:20 -0600

    V513

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gestor-cgs-v512';
+const CACHE = 'gestor-cgs-v513';
 const FILES = ['./GestorPedidosCGS.html', './manifest.json', './GP_activ.png', './GP_spalsh.png', './favicon.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
